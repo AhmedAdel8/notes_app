1691162d7e416babedaa4f661d4b3270825a0340
Ahmed Adel <dola9240@gmail.com>
2024-07-21 18:53:27 +0300

    trigger add note cubit

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,4 +1,3 @@
-
 import 'package:hive/hive.dart';
 
 part 'note_model.g.dart';
@@ -8,7 +7,7 @@ class NoteModel extends HiveObject {
   @HiveField(0)
   final String title;
   @HiveField(1)
-  final String subtitle;
+  final String subTitle;
   @HiveField(2)
   final String date;
   @HiveField(3)
@@ -16,7 +15,7 @@ class NoteModel extends HiveObject {
 
   NoteModel(
       {required this.title,
-      required this.subtitle,
+      required this.subTitle,
       required this.date,
       required this.color});
 }
--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/Cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_botton.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
@@ -24,6 +27,9 @@ class _AddNoteFormState extends State<AddNoteForm> {
       autovalidateMode: autovalidateMode,
       child: Column(
         children: [
+          const SizedBox(
+            height: 35,
+          ),
           CustomTextField(
             onSaved: (value) {
               title = value;
@@ -41,12 +47,19 @@ class _AddNoteFormState extends State<AddNoteForm> {
             maxLines: 5,
           ),
           const SizedBox(
-            height: 50,
+            height: 20,
           ),
           CustomButton(
             onTap: () {
               if (formKey.currentState!.validate()) {
                 formKey.currentState!.save();
+
+                var noteModel = NoteModel(
+                    title: title!,
+                    subTitle: subTitle!,
+                    date: DateTime.now().toString(),
+                    color: Colors.blue.value);
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               } else {
                 autovalidateMode = AutovalidateMode.always;
                 setState(() {});
@@ -54,7 +67,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
             },
           ),
           const SizedBox(
-            height: 10,
+            height: 15,
           ),
         ],
       ),
