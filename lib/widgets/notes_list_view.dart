3740cb1097d4d4e2cd659481f3b56b3e116f7514
Ahmed Adel <dola9240@gmail.com>
2024-07-22 04:32:54 +0300

    fix issue

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -7,12 +7,12 @@ class NotesListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemBuilder: (context, index) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 6),
-        child: const NoteItem(),
-      );
-    });
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 6),
+            child: NoteItem(),
+          );
+        });
   }
 }
