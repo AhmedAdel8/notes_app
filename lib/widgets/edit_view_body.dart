e6aca19f14a2a9b7d390af9893a94a34891246e0
Ahmed Adel <dola9240@gmail.com>
2024-07-24 04:33:13 +0300

    last update

--- lib/widgets/edit_view_body.dart
+++ lib/widgets/edit_view_body.dart
@@ -6,7 +6,7 @@ import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/color_list_view.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
-import 'package:notes_app/widgets/edit_note_color_list_view.dart';
+import 'package:notes_app/widgets/edit_note_colors_list_view.dart';
 
 class EditNoteViewBody extends StatefulWidget {
   const EditNoteViewBody({super.key, required this.note});
